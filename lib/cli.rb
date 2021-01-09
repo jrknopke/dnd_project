5e8e4b1a2689e34c094961fc8f7594f77ad288da
Joseph Knopke <josephknopke@gmail.com>
2021-01-08 22:18:18 -0600

    adding search by level and school of magic

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,24 +5,55 @@ class CLI
 
     def initialize
         puts "Welcome to Spell Finder."
-        enter_spell
+        puts "If you would like to see info for a specific spell, type 'Spell'"
+        puts "If you would like to see all spells for a certain level, type 'Level'"
+        puts "If you would like to see all spells of a certain school, type 'School'"
+        get_input
     end
 
-    def enter_spell
-        puts "Please enter a spell from DnD Fifth Edition:"
+    def get_input
+        input = gets.chomp
+        if input == 'Spell'
+            find_by_name
+        elsif input == 'Level'
+            find_by_level
+        elsif input == 'School'
+            find_by_school
+        else
+            puts "Invalid command. Please enter valid command."
+            get_input
+        end
+    end
+
+    def find_by_name
+        puts "Please enter a spell name:"
         spell_name = gets.chomp
         puts "Thanks. Gathering information about your Spell..."
-        get_info(spell_name)
+        get_spell(spell_name)
     end
 
-    def get_info(spell_name)
+    def find_by_level
+        puts "Please enter the level you would like to see spells for (as a number):"
+        level = gets.chomp
+        puts "Thanks. Gathering list of spells for level #{level}..."
+        get_all_spells_at_level(level)
+    end
+
+    def get_spell(spell_name)
         new_spell = GetSpell.new(spell_name)
-        @response_hash = new_spell.get
-        @spell = Spell.new(@response_hash)
-        display_info
+        response_hash = new_spell.get_spell
+        @spell = Spell.new(response_hash)
+        display_spell_info
     end
 
-    def display_info
+    def get_spells_by_level(Level)
+        list = GetSpell.new(level)
+        response_hash = list.get_spells_by_level
+        
+
+    end
+
+    def display_spell_info
         puts "Name: #{@spell.name}"
         puts "Description: #{@spell.desc}"
         puts "Damage: #{@spell.damage}"
--- lib/get_spell.rb
+++ lib/get_spell.rb
@@ -1,14 +1,24 @@
 require_relative "../config/environment"
 
 class GetSpell
-    
-    def initialize(spell_name)
-        @spell_name = spell_name.downcase.gsub(" ", "-")
+    attr_accessor :input
+
+    def initialize(input)
+        @input = input
         # self.get
     end
 
-    def get
-        url = "https://www.dnd5eapi.co/api/spells/#{@spell_name}"
+    def get_spell
+        @input = @input.downcase.gsub(" ", "-")
+        url = "https://www.dnd5eapi.co/api/spells/#{@info}"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response_hash = JSON.parse(response.body)
+    end
+
+    def get_spells_by_level
+        @input = @input.to_i
+        url = "https://www.dnd5eapi.co/api/spells?level=#{@input}"
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         response_hash = JSON.parse(response.body)
